ab10d32c751804c913bf03985acb4b38592a7ce2
maxwelljkahn <maxwelljkahn@gmail.com>
2021-08-19 16:25:33 -0700

    stubbed up playlist create, update, delete

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,32 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from .models import Song
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Playlist, Song
 
 # Create your views here.
+class PlaylistCreate(CreateView):
+    model = Playlist
+    fields = ["title", "song"]
+
+
+class PlaylistUpdate(UpdateView):
+    model = Playlist
+    fields = ["title", "song"]
+
+
+class PlaylistDelete(DeleteView):
+    model = Playlist
+    success_url = "/playlists/"
+
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def songs_index(request):
     songs = Song.objects.all()
-    return render(request, 'songs/index.html', { 'songs': songs})
+    return render(request, "songs/index.html", {"songs": songs})
