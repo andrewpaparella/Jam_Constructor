8271fb02bbc06be1ea91a800b1e4911f28bba832
AndrewPaparella <paparella.andrew@gmail.com>
2021-08-19 15:15:04 -0700

    stubs up song model, creates main_app url.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    def __string__(self):
+        return self.name
