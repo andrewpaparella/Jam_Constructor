96cdeca82922efc11a1d469f809101c7183f4506
AndrewPaparella <paparella.andrew@gmail.com>
2021-08-19 17:03:00 -0700

    registers models in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Song, Playlist, ReviewPlaylist
 # Register your models here.
+
+admin.site.register(Song)
+admin.site.register(Playlist)
+admin.site.register(ReviewPlaylist)
